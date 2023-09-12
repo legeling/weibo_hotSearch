e95acb74967f2a1f0d66fbc842c5d55e7da10d5a
legeling <w646645452@gmai.com>
2023-09-12 09:38:15 +0800

    修改

--- weibo/test.py
+++ weibo/test.py
@@ -24,8 +24,7 @@ def decoding():
     if not data:
         print('获取微博热搜榜失败')
         return
-    print(datetime.now().strftime('%y年%m月%d日 %H:%M'))
-    # print(f"置顶:{data['hotgov']['word'].strip('#')}")
+    top = (f"置顶:{data['hotgov']['word'].strip('#')}")
     hot_li = []
     hot_label = []
     for i, rs in enumerate(data['realtime'][:20], 1):
@@ -46,10 +45,10 @@ def decoding():
 def img(li,label):
     # 创建图像
     width= 750
-    height = 350 + len(li)*100
+    height = 350 + len(li)*60
     background = Image.new('RGB', (width, height), color=(255, 255, 255))
     # 添加背景图片（如果需要替代顶部像素的背景）
-    background_image = Image.open('hot_research.jpg')  # 替换为你的背景图片
+    background_image = Image.open('resource/hot_research.jpg')  # 替换为你的背景图片
     background.paste(background_image, (0, 0))
 
     # 创建一个空白图像
@@ -60,17 +59,21 @@ def img(li,label):
     background_color = (255, 255, 255)  # 背景颜色
     separator_color = (200, 200, 200)  # 分隔符颜色
     separator_height = 2  # 分隔符高度
-
     image_height = num_lines * line_height  # 图像高度
 
+    # 字体配置
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype("heiti.ttf", font_size)
-    num_font = ImageFont.truetype("SmileySans.ttf", font_size)
+    font = ImageFont.truetype("resource/heiti.ttf", font_size)
+    num_font = ImageFont.truetype("resource/SmileySans.ttf", font_size)
     # 生成文本列表
     lines = li
-
+    # 获取当前时间
+    time = datetime.now().strftime('20%y年 %m月 %d日 %H:%M  %A')
+    time_name = datetime.now().strftime('20%y年%m月%d日%H:%M')
+    draw.text((130, 360), str(time), fill=(101, 109, 118), font=font,font_size=24)
+    draw.rectangle([(0, 400), (width, 400+ separator_height)], fill=separator_color)
     # 逐行绘制文本和分隔符
-    y = 370
+    y = 420
     i = 1
     for line in lines:
         # 绘制编号
@@ -78,7 +81,7 @@ def img(li,label):
         # 绘制文本
         draw.text((130, y),line, fill=text_color, font=font)
         # 绘制热度
-        draw.text((330, y), label[i-1], fill=text_color, font=font)
+        draw.text((700, y), label[i-1], fill=(255,0,0), font=num_font)
         y += line_height
         i +=1
         # 绘制分隔符
@@ -86,7 +89,11 @@ def img(li,label):
         y += separator_height
 
     # 保存图像
-    background.save("output.png")
+    try :
+        background.save(f"archive/{time_name}.png")
+        print("保存成功！")
+    except:
+        print("保存失败！！！")
 
 if __name__ == "__main__":
     hot_li = decoding()[0]
